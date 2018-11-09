9eb641d38efec5b91ac607c3ce5b859079495e10
e <chencoolboy@163.com>
2018-11-09 09:04:26 +0800

    initial version

---
+++ Untitled1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+typedef struct bs 
+{
+int a:8;
+int b:2;
+int c:6;
+ 
+}data;
+ 
+int d = 0xabcd;
+void f1()
+{
+    data  *da = (data *)(&d);
+ 
+    printf("%x %x %x \n",da->a & 0xff ,da->b & 0x3 ,da->c &0x3f);
+}
+void f2()
+{
+    data da;
+    da.a = d & 0xff;
+    da.b = d >> 8 & 0x3;
+    da.c = d >> (8+2) & 0x3f;
+ 
+     
+    printf("%x %x %x \n", da.a & 0xff, da.b & 0x3, da.c & 0x3f);
+}
+int main()
+{
+    f1();
+    f2();
+    return 0;
+}
